a32fa0826a2cbcfce534e47fbb9f0946c30cccfd
yichao0319 <yichao0319@gmail.com>
2015-07-12 11:24:31 -0500

    store/group dependency

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,5 @@
 class Group < ActiveRecord::Base
-    ## Automatic deletion of join models is direct, no destroy callbacks are triggered.
-    has_many :group_sets
+    has_many :group_sets, dependent: :destroy
     has_many :store_infos, :through => :group_sets
     validates :name, presence: true
 end
--- app/models/store_info.rb
+++ app/models/store_info.rb
@@ -1,6 +1,5 @@
 class StoreInfo < ActiveRecord::Base
-    ## Automatic deletion of join models is direct, no destroy callbacks are triggered.
-    has_many :group_sets
+    has_many :group_sets, dependent: :destroy
     has_many :groups, :through => :group_sets
     validates :name, presence: true
 end
